cbaa539a25c3c8bb596ffad1a930382929a78e24
Thurinum <m.gagnon941@gmail.com>
2025-07-11 21:04:06 -0400

    feat: Add Fabric 1.21.7 template w kotlin and data generation

---
+++ src/client/kotlin/org/solarharmony/RiseOfTheVillagersClient.kt
@@ -0,0 +1,9 @@
+package org.solarharmony
+
+import net.fabricmc.api.ClientModInitializer
+
+object RiseOfTheVillagersClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/org/solarharmony/RiseOfTheVillagersDataGenerator.kt
@@ -0,0 +1,9 @@
+package org.solarharmony
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object RiseOfTheVillagersDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
