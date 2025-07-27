0bec34fefa6368266d8ef6a7a74c46cfa281a467
Atlearia <ronanthenickname@gmail.com>
2025-07-27 03:56:14 +0200

    iron helmet here, but nto rendering #THE PillagerEntityRendererMixin.java is COMPLETELY BROKEN ningnong

---
+++ src/client/java/NingNongEntityRenderer.kt
@@ -0,0 +1,6 @@
+import net.minecraft.client.render.entity.EntityRendererFactory
+import net.minecraft.client.render.entity.PillagerEntityRenderer
+
+class NingNongEntityRenderer(context: EntityRendererFactory.Context) : PillagerEntityRenderer(context) {
+
+}
--- src/client/kotlin/org/solarharmony/RiseOfTheVillagersClient.kt
+++ src/client/kotlin/org/solarharmony/RiseOfTheVillagersClient.kt
@@ -1,5 +1,6 @@
 package org.solarharmony
 
+import NingNongEntityRenderer
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry
 import net.minecraft.client.render.entity.PillagerEntityRenderer
