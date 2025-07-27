9ea2843f3769da4de812b018ccb4e94a7b552dd0
Atlearia <ronanthenickname@gmail.com>
2025-07-27 16:19:57 +0200

    before break

--- src/client/java/NingNongEntityRenderer.java
+++ src/client/java/NingNongEntityRenderer.java
@@ -1,6 +1,12 @@
-import net.minecraft.client.render.entity.EntityRendererFactory
-import net.minecraft.client.render.entity.PillagerEntityRenderer
 
-class NingNongEntityRenderer(context: EntityRendererFactory.Context) : PillagerEntityRenderer(context) {
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.PillagerEntityRenderer;
 
+public class NingNongEntityRenderer extends PillagerEntityRenderer {
+
+    public NingNongEntityRenderer(EntityRendererFactory.Context context) {
+        super(context);
+    }
 }
