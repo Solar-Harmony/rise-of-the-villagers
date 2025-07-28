1d38150bd029dae16b490bcf7e02ea9a124bf53d
Atlearia <ronanthenickname@gmail.com>
2025-07-28 05:36:56 +0200

    before copying ez

--- src/client/java/entityrenderer/IllagerArmorFeatureRenderer.java
+++ src/client/java/entityrenderer/IllagerArmorFeatureRenderer.java
@@ -1,86 +1,140 @@
 package entityrenderer;
 
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
-import net.minecraft.client.render.entity.model.EntityModelLoader;
 import net.minecraft.client.render.entity.model.IllagerEntityModel;
 import net.minecraft.client.render.entity.state.IllagerEntityRenderState;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.mob.IllagerEntity;
-import net.minecraft.client.render.entity.model.EntityModel;
-import net.minecraft.client.render.entity.model.ModelWithArms;
-import net.minecraft.client.render.entity.model.ModelWithHead;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
 import net.minecraft.util.Identifier;
-import net.minecraft.client.render.entity.model.ModelPart;
-import net.minecraft.client.render.EquipmentRenderer;
-import net.minecraft.client.render.entity.model.BipedEntityModel.ArmPose;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.decoration.ArmorStandEntity;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.client.model.Dilation;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
-public class IllagerArmorFeatureRenderer extends ArmorFeatureRenderer<IllagerEntity, IllagerEntityModel<IllagerEntityRenderState>> {
+@Environment(EnvType.CLIENT)
+public class IllagerArmorFeatureRenderer extends FeatureRenderer<IllagerEntityRenderState, IllagerEntityModel<IllagerEntityRenderState>> {
 
-    private final IllagerEntityModel<IllagerEntityRenderState> innerModel;
-    private final IllagerEntityModel<IllagerEntityRenderState> outerModel;
+    private static final Identifier ARMOR_TEXTURE = new Identifier("textures/models/armor/iron_layer_1.png");
+
+    private final BipedEntityModel<IllagerEntityRenderState> innerArmorModel;
+    private final BipedEntityModel<IllagerEntityRenderState> outerArmorModel;
 
     public IllagerArmorFeatureRenderer(
-            FeatureRendererContext<IllagerEntity, IllagerEntityModel<IllagerEntityRenderState>> context,
-            IllagerEntityModel<IllagerEntityRenderState> innerModel,
-            IllagerEntityModel<IllagerEntityRenderState> outerModel,
-            EquipmentRenderer equipmentRenderer) {
-        super(context, createBipedArmorModel(0.5F), createBipedArmorModel(1.0F), equipmentRenderer);
-        this.innerModel = innerModel;
-        this.outerModel = outerModel;
+            FeatureRendererContext<IllagerEntityRenderState, IllagerEntityModel<IllagerEntityRenderState>> context,
+            BipedEntityModel<IllagerEntityRenderState> innerArmorModel,
+            BipedEntityModel<IllagerEntityRenderState> outerArmorModel) {
+        super(context);
+        this.innerArmorModel = innerArmorModel;
+        this.outerArmorModel = outerArmorModel;
     }
 
-    // Create placeholder biped models to satisfy the parent constructor
-    private static BipedEntityModel createBipedArmorModel(float scale) {
-        return new BipedEntityModel(Dilation.NONE);
-    }
-
-    // Override the setVisible method to use illager model parts
     @Override
-    protected void setVisible(IllagerEntityModel<IllagerEntityRenderState> model, EquipmentSlot slot, boolean visible) {
+    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light,
+                       IllagerEntityRenderState state, float limbAngle, float limbDistance) {
+        // Copy state from illager model to armor models
+        copyStateToModel(state, this.innerArmorModel);
+        copyStateToModel(state, this.outerArmorModel);
+
+        // Get the corresponding IllagerEntity from the state
+        // This would need to be adapted based on how your state system works
+        IllagerEntity illager = getIllagerEntityFromState(state);
+        if (illager == null) return;
+
+        // Render each equipment slot
+        for (EquipmentSlot slot : EquipmentSlot.values()) {
+            if (slot.getType() != EquipmentSlot.Type.ARMOR) continue;
+
+            ItemStack itemStack = illager.getEquippedStack(slot);
+            if (!(itemStack.getItem() instanceof ArmorItem)) continue;
+
+            ArmorItem armorItem = (ArmorItem)itemStack.getItem();
+            if (armorItem.getSlotType() != slot) continue;
+
+            BipedEntityModel<IllagerEntityRenderState> model = slot == EquipmentSlot.LEGS ?
+                    this.innerArmorModel : this.outerArmorModel;
+
+            // Set visibility based on slot
+            setArmorPartVisibility(model, slot);
+
+            // Get proper texture for the armor
+            Identifier texture = getArmorTexture(armorItem, slot);
+
+            // Render the armor piece
+            renderArmorPiece(matrices, vertexConsumers, light, illager, limbAngle,
+                    limbDistance, model, texture);
+        }
+    }
+
+    private void copyStateToModel(IllagerEntityRenderState state, BipedEntityModel<IllagerEntityRenderState> model) {
+        // Update armor model with illager animation state
+        model.handSwingProgress = state.handSwingProgress;
+        model.riding = state.hasVehicle;
+        model.child = false;
+
+        // Set arm pose based on state
+        if (state.attacking) {
+            model.rightArmPose = BipedEntityModel.ArmPose.ATTACK;
+            model.leftArmPose = BipedEntityModel.ArmPose.ATTACK;
+        } else {
+            model.rightArmPose = BipedEntityModel.ArmPose.EMPTY;
+            model.leftArmPose = BipedEntityModel.ArmPose.EMPTY;
+        }
+    }
+
+    private void setArmorPartVisibility(BipedEntityModel<IllagerEntityRenderState> model, EquipmentSlot slot) {
         model.setVisible(false);
         switch (slot) {
             case HEAD:
-                model.getHead().visible = visible;
+                model.head.visible = true;
+                model.hat.visible = true;
                 break;
             case CHEST:
-                model.getBody().visible = visible;
-                model.getRightArm().visible = visible;
-                model.getLeftArm().visible = visible;
+                model.body.visible = true;
+                model.rightArm.visible = true;
+                model.leftArm.visible = true;
                 break;
             case LEGS:
-                model.getBody().visible = visible;
-                model.getRightLeg().visible = visible;
-                model.getLeftLeg().visible = visible;
+                model.body.visible = true;
+                model.rightLeg.visible = true;
+                model.leftLeg.visible = true;
                 break;
             case FEET:
-                model.getRightLeg().visible = visible;
-                model.getLeftLeg().visible = visible;
+                model.rightLeg.visible = true;
+                model.leftLeg.visible = true;
+                break;
+            default:
                 break;
         }
     }
 
-    // Override other methods as needed to render the armor on the illager model
-    @Override
-    protected void setPartVisibility(IllagerEntityModel<IllagerEntityRenderState> model, EquipmentSlot slot) {
-        setVisible(model, slot, true);
+    // Helper method to get illager entity from state
+    // This would need to be implemented based on your system
+    private IllagerEntity getIllagerEntityFromState(IllagerEntityRenderState state) {
+        // This is a placeholder - you would need to implement this
+        // based on how your rendering system works
+        return null;
     }
 
-    // Override render method to use the illager models instead of biped models
-    @Override
-    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light,
-                       IllagerEntity entity, float limbAngle, float limbDistance,
-                       float tickDelta, float animationProgress, float headYaw, float headPitch) {
-        // Custom rendering logic that uses innerModel and outerModel
-        // This would be similar to the parent class but adapted for illager models
+    // Helper method to get armor texture
+    private Identifier getArmorTexture(ArmorItem armorItem, EquipmentSlot slot) {
+        // This is simplified - normally you would determine the texture based on material and slot
+        return ARMOR_TEXTURE;
+    }
+
+    // Helper method to render armor piece
+    private void renderArmorPiece(MatrixStack matrices, VertexConsumerProvider vertexConsumers,
+                                  int light, IllagerEntity illager, float limbAngle,
+                                  float limbDistance, BipedEntityModel<IllagerEntityRenderState> model,
+                                  Identifier texture) {
+        // This is simplified - normally you would handle coloring, glint effects, etc.
+        model.render(matrices, vertexConsumers.getBuffer(model.getLayer(texture)),
+                light, ItemRenderer.getItemGlintConsumer(vertexConsumers, model.getLayer(texture),
+                        false, false), 1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
